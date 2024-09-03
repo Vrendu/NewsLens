e60858176496419e9f051759f19862c5718812ae
Vamshi Renduchintala <93363393+Vrendu@users.noreply.github.com>
2024-09-02 18:22:00 -0700

    added web frontend and backend, with user auth

--- newslens/public/contentScript.js
+++ newslens/public/contentScript.js
@@ -1,15 +1,19 @@
-(() => {
-    try {
-        const title = document.title;
-        const content = document.body.innerText.slice(0, 50000); // Get the first 50,000 characters
+//contentScript.js
 
-        if (chrome.runtime && chrome.runtime.sendMessage) {
-            // Send the title and content to the background script or popup
-            chrome.runtime.sendMessage({ title, content });
-        } else {
-            console.error('chrome.runtime or sendMessage is not available.');
+(() => {
+   // document.addEventListener('DOMContentLoaded', () => {
+        try {
+            const title = document.title;
+            const content = document.body.innerText.slice(0, 50000); // Get the first 50,000 characters
+
+            if (chrome.runtime && chrome.runtime.sendMessage) {
+                // Send the title and content to the background script or popup
+                chrome.runtime.sendMessage({ title, content });
+            } else {
+                console.error('chrome.runtime or sendMessage is not available.');
+            }
+        } catch (error) {
+            console.error('Error in content script:', error);
         }
-    } catch (error) {
-        console.error('Error in content script:', error);
-    }
+  //  });  
 })();
