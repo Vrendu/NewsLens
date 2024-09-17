759c8d7ee02b01dfdf3f8217f7194bd11d73a78e
Vamshi Renduchintala <93363393+Vrendu@users.noreply.github.com>
2024-09-16 18:35:06 -0700

    dataset address fix

--- extension_frontend/public/background.js
+++ extension_frontend/public/background.js
@@ -12,20 +12,19 @@ const domainPatterns = [
         pattern: /^https:\/\/www\.cnn\.com/,
         publication: "CNN (Online News)"
     },
+    {
+        pattern: /^https:\/\/www\.foxnews\.com/,
+        publication: "Fox News (Online News)"
+    },
     // Matches anything that comes after foxnews.com/opinion
     {
         pattern: /^https:\/\/www\.foxnews\.com\/opinion/,
         publication: "Fox News (Opinion)"
     },
-    // Matches anything that comes after foxbusiness.com
+
     {
         pattern: /^https:\/\/www\.foxbusiness\.com/,
         publication: "Fox News Business"
-    },
-    // Matches any other URL that comes after foxnews.com
-    {
-        pattern: /^https:\/\/www\.foxnews\.com/,
-        publication: "Fox News (Online News)"
     }
 ];
 
@@ -62,7 +61,7 @@ chrome.runtime.onMessage.addListener(async (message) => {
 
             // Fetch the bias data from the AllSides dataset
             try {
-                const response = await fetch("https://storage.googleapis.com/kagglesdsdata/datasets/1491526/2641594/allsides.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240914%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240914T005736Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=7693fce5165af3a2a4abb76dacb839a1547bb52c903f240fa1b84941eab9caac4afed4483172ee9e71eda97e07d6f630d2229aa43eeadf79bce2bdcd10b71ca1d49f566b6f551ee743e462923e113cdb4c3348d15b78baad4445d50a80edd4662d462863321348944013d899354718fd663e10f436f1623140dd081d5ea15768893e03cb22ff59085d55f00acd07ee0a073d13c917a7c5cdb5e0f25a959fa336b74596cadb2b7990b35a68f4c4bfa2d97b434c28511ed5f73539c383d46adf2a1130c0e1592e76ffb7de6e03c7b6af59b4309520fb67628e1fffd9f1a97891552c1158d9e98e624affb61d18cbd4a67f16bc64b5086dfb1418e0c12c025f44b8");
+                const response = await fetch("https://storage.googleapis.com/kagglesdsdata/datasets/1491526/2641594/allsides.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240917%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240917T013139Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=07cf353b19bf206712e8f156fd0edd0d288b61c978f71238e01d7d21a3bb70e9ca7633080bf372c1ff579fc5a41ff9182243d360d12dfafe8776c6af78a1ce00cdc9e5d1ad355f5e5cf0a1dd027a94ee517fd95c4a42593ada28c6aa10d08fe8544019c09e767fd3f2074a757f7a308624b7b9a0507dce28ce7db415b1315d6b90304c61f79a84d82d89b62f98ca9580e955acaff307c3de73c7ebaa6b3334226681d9f7dea32f5f1f3e8ebd1447eca6a5a51dc91ff1bc8fce31efac19ab25bc10c25ede06fabe2b966aa09ad28db16bcc15a3136e9eed24dd1784fc6a2e30cd59a72fa1b6a3fe1fe478e3d6b8ca1fa3b6d92db315e828f150b1bb33897c7235");
                 if (!response.ok) {
                     throw new Error("Failed to fetch bias data.");
                 }
